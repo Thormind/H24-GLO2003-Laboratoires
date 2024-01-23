95cb44d0d1ad2bd52426a0c92bbdfbb84bc34b52
Frederic Belanger <frederic.belanger.6@ulaval.ca>
2024-01-23 17:54:21 -0500

    lab1

--- src/main/java/ca/ulaval/glo2003/HealthResource.java
+++ src/main/java/ca/ulaval/glo2003/HealthResource.java
@@ -1,10 +1,7 @@
 package ca.ulaval.glo2003;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
 
 @Path("")
 public class HealthResource {
@@ -16,5 +13,62 @@ public class HealthResource {
     {
         System.out.println("Hello, world!");
         return Response.ok(new HealthResponse("ok")).build();
+    }
+
+    @GET
+    @Path("products")
+    public void products(@QueryParam("sort") String sort, @Context UriInfo uriInfo)
+    {
+        if(uriInfo.getQueryParameters().getFirst("sort").equals("popular"))
+        {
+            System.out.println("Popular sort");
+        }
+        else
+        {
+            System.out.println("another sort!");
+        }
+    }
+
+    @GET
+    @Path("products2")
+    public void products2(@QueryParam("sort") String sort, @HeaderParam ("Content-Type") String content,
+                          @PathParam("id") String id)
+    {
+        if(sort.equals("popular"))
+        {
+            System.out.println("Popular sort");
+        }
+        else
+        {
+            System.out.println("another sort!");
+        }
+
+        System.out.println(content);
+        System.out.println(id);
+    }
+
+    @GET
+    @Path("products3/{id}")
+    public void products3(@QueryParam("sort") String sort, @HeaderParam ("Content-Type") String content,
+                          @PathParam("id") String id)
+    {
+        if(sort.equals("popular"))
+        {
+            System.out.println("Popular sort");
+        }
+        else
+        {
+            System.out.println("another sort!");
+        }
+
+        System.out.println(content);
+        System.out.println(id);
+    }
+
+    @POST
+    @Path("products4/{id}")
+    public void products4()
+    {
+
     }
 }
